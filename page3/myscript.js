9e40408c41dd9485334201a44d7d4f27df943809
yukikohiga-sys <75060610+yukikohiga-sys@users.noreply.github.com>
2021-08-04 18:04:05 +0900

    Add files via upload

--- page3/myscript.js
+++ page3/myscript.js
@@ -4,7 +4,7 @@ document.getElementById('target1').addEventListener('click', () => {
     document.getElementById('target1').classList.toggle('circle1');
   });
 document.getElementById('target2').addEventListener('click', () => {
-    document.getElementById('target2').classList.toggle('circle2');
+    document.getElementById('target2').classList.toggle('circle3');
   });
 document.getElementById('target3').addEventListener('click', () => {
     document.getElementById('target3').classList.toggle('circle3');
